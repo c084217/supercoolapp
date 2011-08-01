542bc6fece1121cde63bf821d0fac6bf3d68d475
shiroshana <shiroshana@gmail.com>
2011-08-01 09:46:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Supercoolapp::Application.config.secret_token = '659731659c0a0b6fbfb5c89a7b44db45b721764ed14ef5b1e528a4e359d7ce63e1edb46bf07df525debe64570b29fc90c94878c3bdf8768a6b65b836889f7ead'
